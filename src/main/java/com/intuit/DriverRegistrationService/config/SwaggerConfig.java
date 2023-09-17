e8f3f9fd920ed388556fdffed835f9d685ec392a
rikkz <mangla.rohit730@gmail.com>
2023-09-18 02:26:31 +0530

    Updating the gitIgnore / Adding build.gradle dependencies

--- src/main/java/com/intuit/DriverRegistrationService/config/SwaggerConfig.java
+++ src/main/java/com/intuit/DriverRegistrationService/config/SwaggerConfig.java
@@ -4,11 +4,23 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for Swagger documentation.
+ * This class sets up the Swagger documentation for the "Driver Registration Service" module
+ * of the application, providing metadata about the service.
+ */
 @Configuration
 public class SwaggerConfig {
 
+    /**
+     * Defines the Swagger OpenAPI documentation for the Driver Registration Service.
+     *
+     * @return An OpenAPI object containing metadata about the service.
+     */
     @Bean
     public OpenAPI usersMicroserviceOpenAPI() {
+        // Create an instance of OpenAPI and configure its metadata
         OpenAPI info;
         info = new OpenAPI()
                 .info(new Info().title("Driver Registration Service")
