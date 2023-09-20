1656343e5c3b576a4bc5edd116816d8e8f586f52
rikkz <mangla.rohit730@gmail.com>
2023-09-21 03:39:39 +0530

    adding remaning documentation

--- src/main/java/com/intuit/DriverRegistrationService/repository/DriverRepository.java
+++ src/main/java/com/intuit/DriverRegistrationService/repository/DriverRepository.java
@@ -5,7 +5,18 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
+/**
+ * The {@code DriverRepository} interface provides methods for interacting with the driver data
+ * stored in the MongoDB database.
+ */
 public interface DriverRepository extends MongoRepository<DriverDataModel, String> {
 
+    /**
+     * Find drivers by their country code and mobile number.
+     *
+     * @param countryCode   The country code associated with the mobile number.
+     * @param mobileNumber  The mobile phone number of the driver.
+     * @return A list of {@link DriverDataModel} objects matching the given country code and mobile number.
+     */
     List<DriverDataModel> findByCountryCodeAndMobileNumber(String countryCode, String mobileNumber);
 }
